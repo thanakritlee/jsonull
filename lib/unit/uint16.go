4c42e390605343622ae21af5466cbeab0209bd4c
Supachai Keemthong <supachai@smartcontractthailand.com>
2019-09-08 20:14:12 +0700

    Add library uint

---
+++ lib/unit/uint16.go
@@ -0,0 +1,48 @@
+package uint
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+// Uint16 struct.
+type Uint16 struct {
+	Value uint16
+	Valid bool
+	Set   bool
+}
+
+// UnmarshalJSON unmarshaler implementation for Uint16.
+func (j *Uint16) UnmarshalJSON(data []byte) error {
+	// If this method is call, the value is set.
+	// Value could be set to either null or some non-null value.
+	j.Set = true
+
+	if string(data) == "null" {
+		// The value is set to null.
+		j.Valid = false
+		return nil
+	}
+
+	// The value isn't set to null.
+	var temp uint16
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+	j.Value = temp
+	j.Valid = true
+	return nil
+}
+
+// MarshalJSON marshaler implementation for Float16.
+func (j Uint16) MarshalJSON() ([]byte, error) {
+	buffer := bytes.NewBufferString("")
+	if j.Valid {
+		buffer.WriteString(fmt.Sprintf("%f", j.Value))
+	} else {
+		buffer.WriteString("null")
+	}
+
+	return buffer.Bytes(), nil
+}
---
+++ lib/unit/uint32.go
@@ -0,0 +1,48 @@
+package uint
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+// Uint32 struct.
+type Uint32 struct {
+	Value uint32
+	Valid bool
+	Set   bool
+}
+
+// UnmarshalJSON unmarshaler implementation for Uint32.
+func (j *Uint32) UnmarshalJSON(data []byte) error {
+	// If this method is call, the value is set.
+	// Value could be set to either null or some non-null value.
+	j.Set = true
+
+	if string(data) == "null" {
+		// The value is set to null.
+		j.Valid = false
+		return nil
+	}
+
+	// The value isn't set to null.
+	var temp uint32
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+	j.Value = temp
+	j.Valid = true
+	return nil
+}
+
+// MarshalJSON marshaler implementation for Float32.
+func (j Uint32) MarshalJSON() ([]byte, error) {
+	buffer := bytes.NewBufferString("")
+	if j.Valid {
+		buffer.WriteString(fmt.Sprintf("%f", j.Value))
+	} else {
+		buffer.WriteString("null")
+	}
+
+	return buffer.Bytes(), nil
+}
